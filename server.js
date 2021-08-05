c5fe9e7280d0e539b961fba2ee07534f83d0e422
QPThree <youngqp3@gmail.com>
2021-08-04 20:12:21 -0700

    added movie class. instances are created and sent to front end

--- server.js
+++ server.js
@@ -27,43 +27,52 @@ app.get('/', (request, response) => {
 
 //weather data will route here
 app.get('/weather', async (request, response) => {
-  try{
+  try {
     let forecastArr = [];
-  let cityName = request.query.searchQuery;
-  let lat = request.query.lat;
-  let lon = request.query.lon;
-  //call to weather api
-  const weatherData = await axios.get(`https://api.weatherbit.io/v2.0/forecast/daily?city=${cityName}&key=${process.env.WEATHER_API_KEY}`);
+    let cityName = request.query.searchQuery;
+    let lat = request.query.lat;
+    let lon = request.query.lon;
+    //call to weather api
+    const weatherData = await axios.get(`https://api.weatherbit.io/v2.0/forecast/daily?city=${cityName}&key=${process.env.WEATHER_API_KEY}`);
 
-  forecastArr.push(new Forecast(weatherData.data.data));
-  response.send(forecastArr);
-  } catch (error){
+    forecastArr.push(new Forecast(weatherData.data.data));
+    response.send(forecastArr);
+  } catch (error) {
     response.status(404).send('Something went wrong with the weather data!');
   }
 });
 
 app.get('/movies', async (request, response) => {
-  try{
+  try {
     let cityName = request.query.searchQuery;
-    const movieData = await axios.get(`https://api.themoviedb.org/3/search/movie?uery=${cityName}&api_key=${process.env.MOVIE_API_KEY}&language=en-US&page=1&include_adult=false`);
+    let resultsArr = [];
+    let movieData = await axios.get(`https://api.themoviedb.org/3/search/movie?query=${cityName}&api_key=${process.env.MOVIE_API_KEY}&language=en-US&page=1&include_adult=false`);
     //movie stuff here
-    console.log(movieData.data.results[0].original_title);
-    response.send(movieData.data.results);
-  }catch(error){
+    console.log(movieData.data);
+    movieData.data.results.map(movie => {
+      resultsArr.push(new Movie(movie))
+    });
+    response.send(resultsArr);
+  } catch (error) {
+    console.log(error)
     response.status(404).send('Something went wrong with the movie data!');
   }
 });
 app.get('/*', (request, response) => {
   response.status(404).send('Something went wrong!');
 });
-//need to tellserver wehre to listen!
+//need to tellserver where to listen!
 app.listen(PORT, () => console.log(`listening on port ${PORT}`));
 
 //forecast object has 2 arrays, one for dates and the other for their corresponding weather report
 class Forecast {
   constructor(data) {
-    console.log('new forecast');
     this.threeDayDates = data.map(day => day.datetime);
     this.threeDayDescription = data.map(day => day.weather.description);
+  }
+}
+class Movie {
+  constructor(data) {
+    this.data = data;
   }
 }
