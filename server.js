de38bcd87b17e069c8752d77563c59e3cdee0a2f
QPThree <youngqp3@gmail.com>
2021-08-10 21:27:36 -0700

    new server.js is up and running correctly locally

--- server.js
+++ server.js
@@ -1,41 +1,25 @@
 'use strict';
 
-console.log('Got Pats, from our Server!');
-
-//require is essential an import
+require('dotenv').config(); //had to add .config();
 const express = require('express');
-require('dotenv').config();
-
-//express must be called to be used as per docs
-const app = express();
-
-//cors for front end to talk to back end!
 const cors = require('cors');
-app.use(cors());
 
-//hard wired port from .env
-const PORT = process.env.PORT;
+const weather = require('./modules/weather.js');
+const app = express();
+app.use(cors()); //had to add this
 
-// -----Modules requires ---
+app.get('/weather', weatherHandler);
 
-const Weather = require('./modules/weather.js');
-const Movies = require('./modules/movies.js');
+function weatherHandler(request, response) {
+  // const { lat, lon } = request.query;
+  const lat = request.query.lat;
+  const lon = request.query.lon;
+  weather(lat, lon)
+    .then(summaries => response.send(summaries))
+    .catch((error) => {
+      console.error(error);
+      response.status(200).send('Sorry. Something went wrong!')
+    });
+}
 
-//----GET REQUESTS-----
-//specify routes our server should be listening for
-//this is a send so it is displayed on browser! (console.logs are displayer in console and terminal)
-app.get('/', (request, response) => {
-  response.send('Hello again from the server response.send!');
-});
-
-//weather data will route here
-app.get('/weather', Weather);
-
-app.get('/movies', Movies);
-app.get('/*', (request, response) => {
-  response.status(404).send('Something went wrong!');
-});
-//need to tellserver where to listen!
-app.listen(PORT, () => console.log(`listening on port ${PORT}`));
-
-
+app.listen(process.env.PORT, () => console.log(`Server up on ${process.env.PORT}`));
